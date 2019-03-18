5517985b9354890518c780eb326a491ab08b8e54
ruhlelana <47975919+ruhlelana@users.noreply.github.com>
2019-03-19 03:54:05 +0700

    Error Fullpage 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
 <div>
-  <full-page ref="fullpage" id="fullpage">
-  <div class="section">
-    Halaman Satu
-  </div>
-  <div class="section">
-    Halaman Dua
-  </div>
-</full-page>
+    <full-page ref="fullpage" :options="options" id="fullpage">
+    <div class="section pemilu">
+      First section ...
+    </div>
+    <div class="section energi">
+      Second section ...
+    </div>
+  </full-page>
 </div>
 </template>
 
@@ -15,4 +15,16 @@
 </script>
 
 <style>
+.pemilu {
+ background-image: url(/img/pemilu.jpg);
+ background-position: center center;
+ background-repeat: no-repeat;
+ background-size: cover;
+}
+.energi {
+ background-image: url(/img/energi.jpg);
+ background-position: center center;
+ background-repeat: no-repeat;
+ background-size: cover;
+}
 </style>
